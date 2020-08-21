ff21911ea5629b5d0ee768c6dd418e0320d013d1
xtellaris <x86armv7@gmail.com>
2020-08-21 19:15:01 +0800

    Fixed exception caused by Paths with spaces

--- ffmpeg/Transcoder.sh
+++ ffmpeg/Transcoder.sh
@@ -8,16 +8,16 @@ if [ ! -d "$1" ]; then
 fi
 WorkFolder="$1";
 # Specify OutFolder
-mkdir $WorkFolder/OUTPUT
+mkdir "$WorkFolder"/OUTPUT
 OutFolder="$WorkFolder/OUTPUT"
 
 # File_AN=files' absolute path and name
 # File_BN=files' base name
-for File_AN in ${WorkFolder}/*
+for File_AN in "${WorkFolder}"/*
 do
-	if [ -f $File_AN ]; then
-		File_BN=$(basename $File_AN);
-		File_OUT="$OutFolder/$File_BN.mp4";
+	if [ -f "$File_AN" ]; then
+		File_BN=$(basename "$File_AN");
+		File_OUT="$OutFolder"/"$File_BN".mp4;
 		ffmpeg -hwaccel qsv -ss 00:00:06.35 -i "$File_AN" -c:v hevc_nvenc -preset slow -profile:v main -b:v 1000K -c:a libfdk_aac -b:a 64K -vf eq=brightness=-0.03 "$File_OUT";	
 	fi
 done
